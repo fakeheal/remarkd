8cc01bebaa6d4891ffc8ec9319560e3041e8b351
Ivanka Todorova <i.todorova@dineout.bg>
2021-06-30 17:16:03 +0300

    Include Tag's id in TagResource.php

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -15,6 +15,7 @@ class TagResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id'=> $this->id,
             'name' => $this->name,
             'description' => $this->description
         ];
